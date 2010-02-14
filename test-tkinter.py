9704dae4051c0c9ee3e582d75a9428af926cfdcb
Sense <coding.src@gmail.com>
2010-02-14 20:55:55 +0100

    Spam window avec tkinter

---
+++ test-tkinter.py
@@ -0,0 +1,16 @@
+#-*- coding utf-8 -*-
+# Nom : Spam window
+# Auteur : Sense
+
+from Tkinter import *
+
+def ouverture():
+	fenetre = Tk()
+	text1 = Label(fenetre, text=' \o/', fg='gray')
+	text1.pack()
+	bout1 = Button(fenetre, text='Quitter', command = fenetre.destroy)
+	bout1.pack()
+	fenetre.mainloop()
+	
+while 1 != 0:
+	ouverture()
